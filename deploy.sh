51f322e197a9adc2e299c687afbe15fedbc3c528
lenyuqin <1151475385@qq.com>
2021-03-28 23:03:23 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,15 +11,15 @@ cd docs/.vuepress/dist
 
 # deploy to github
 echo 'interview.limingkang.com' > CNAME
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com:lenyuqin/interview.git
-else
-  msg='来自github actions的自动部署'
-  githubUrl=https://lenyuqin:${GITHUB_TOKEN}@github.com/lenyuqin/interview.git
-  git config --global user.name "lenyuqin"
-  git config --global user.email "1151475385@qq.com"
-fi
+#if [ -z "$GITHUB_TOKEN" ]; then
+#  msg='deploy'
+#  githubUrl=git@github.com:lenyuqin/interview.git
+#else
+msg='来自github actions的自动部署'
+githubUrl=https://lenyuqin:${GITHUB_TOKEN}@github.com/lenyuqin/interview.git
+git config --global user.name "lenyuqin"
+git config --global user.email "1151475385@qq.com"
+#fi
 git init
 git add -A
 git commit -m "${msg}"
