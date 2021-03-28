c60cd6aa7076bc6d02506ff066ed70a6221c45b7
limingkang <1151475385@qq.com>
2021-03-28 22:30:17 +0800

    init: 项目初始化

---
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -0,0 +1,73 @@
+// nav
+module.exports = [
+  {
+    text: 'MySQL',
+    link: '/mysql/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    items: [
+      { text: '索引', link: '/pages/b487c0/' },
+      { text: 'Java语言', link: '/pages/0b69c8/' },
+      { text: 'C语言', link: '/pages/7426b1/' },
+    ],
+  },
+  {
+    text: '数据库',
+    link: '/database/',
+    items: [
+      { text: 'MySQL', link: '/pages/1aef7c/' },
+      { text: 'Redis', link: '/pages/cbd711/' },
+      { text: 'MongoDB', link: '/pages/cbd712/' },
+    ],
+  },
+  {
+    text: '学科',
+    link: '/base/',
+    items: [
+      { text: '操作系统', link: '/pages/5bfa72/' },
+      { text: '计算机网咯', link: '/pages/6bb966/' },
+    ],
+  },
+  {
+    text: '框架',
+    link: '/frame/',
+    items: [
+      { text: 'spring全家桶', link: '/pages/9baa5c/' },
+      { text: 'mybatis', link: '/pages/eaabb9/' },
+    ],
+  },
+  {
+    text: '软件|系统',
+    link: '/system/',
+    items: [
+      { text: '软件相关', link: '/pages/6bf004/' },
+      { text: '系统相关', link: '/pages/a5d462/' },
+    ],
+  },
+  {
+    text: '算法|数据结构',
+    link: '/algorithm/',
+    items: [
+      { text: '算法', link: '/pages/817a23/' },
+      { text: '数据结构', link: '/pages/cf8bda/' },
+      { text: '设计模式', link: '/pages/d240cd/' },
+      { text: 'CMU硕士经典100题', link: '/pages/413b20/' },
+    ],
+  },
+  {
+    text: '面试',
+    link: '/interview/',
+    items: [
+      { text: '3月', link: '/pages/e7050d/' },
+    ],
+  },
+  {
+    text: '其他',
+    link: '/other/',
+    items: [
+      { text: '智力题', link: '/pages/e6c953/' }
+    ],
+  },
+  {
+    text: 'APP下载',
+    link: 'https://www.yd-mobile.cn/pack/download?versionId=2497&packName=com.xiaoyou.interview'
+  }
+]
